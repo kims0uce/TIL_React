609a37c28a934ec84a16cc835fefa1ea781a9eb0
kims0uce <ksy_59@naver.com>
2024-01-22 07:51:57 +0900

    54.데이터 기반 상태가져오기 및 출력

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,22 @@
-import componentImg from "./assets/components.png";
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TapButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+
+    // 로그 찍어보면 옛날 상태가 나옴
+    // 아직 화면이 재렌더링되기 이전이기 때문
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -16,6 +29,24 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
---
+++ 01-starting-project/src/components/TapButton.jsx
@@ -0,0 +1,11 @@
+export default function TabButton(props) {
+  // children prop은 리액트에서 제공해주는 속성이기 때문에 이름을 그대로 가져가야 함
+  // 다른 props는 사용자가 마음대로 지정할 수 있음
+  const { children, onSelect } = props;
+
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
