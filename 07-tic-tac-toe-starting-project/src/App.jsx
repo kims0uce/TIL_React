f8c40660523059262178737371206d3cb350ed18
kims0uce <ksy_59@naver.com>
2024-01-28 22:11:41 +0900

    79. 다차원 배열 렌더링으로 게임판 보드까지 생성

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,20 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
-  
-
+  // <main>을 wrapper element로 사용하여 각 빌딩블록을 감싼다.
+  // <ol> 태그를 사용한 이유는 플레이어 간 순서가 존재하기 때문이다.
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  // 데이터를 조정하여 UI 수정 후 업데이트 되도록 하기위해 useState 활용한다.
+  // 상태값을 생성함으로써 해당 값을 수정할 수 있는 함수를 가져온다.
+  // 이 함수를 통해 리액트는 UI를 수정한다.
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  // handle 함수가 불리면 해당 컴포넌트가 한번 리렌더링 되고,
+  // 코드에서 변경사항이 존재하면 DOM과 다른 컴포넌트에 반영된다.
+  const handleEditButton = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  // input 태그의 onChange 요소는 매 입력에 의해 발동되고,
+  // 이벤트 객체를 제공하는데 객체의 내용은 이용자가 작성한 값이다.
+  const handleChangeName = (e) => {
+    // .target : 그 이벤트가 방출한 요소를 참조한다.
+    // .value : 이용자가 작성하려고 한 값을 저장한다.
+    setPlayerName(e.target.value);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" value={playerName} onChange={handleChangeName} />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditButton}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
