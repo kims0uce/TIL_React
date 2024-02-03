c5f9a529d718254cf7a58c096e0d84fce9eb21cc
kims0uce <ksy_59@naver.com>
2024-02-03 12:09:14 +0900

    add Header

--- 08-investment-calculator/src/App.jsx
+++ 08-investment-calculator/src/App.jsx
@@ -1,7 +1,7 @@
+import Header from "./components/Header";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
---
+++ 08-investment-calculator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import headerLogo from "@/assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <>
+      <img src="headerLogo" />
+      <h1>Investment Calculator</h1>
+    </>
+  );
+}
