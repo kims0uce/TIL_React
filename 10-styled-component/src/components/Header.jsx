5e86a1da83d4ea1e427aca0e49853df659ea8eb4
kims0uce <ksy_59@naver.com>
2024-03-03 15:47:41 +0900

    update to use inline style

--- 10-styled-component/src/components/Header.jsx
+++ 10-styled-component/src/components/Header.jsx
@@ -1,11 +1,20 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p
+        style={{
+          textAlign: "center",
+          color: "#a39191",
+          margin: 0,
+        }}
+      >
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
