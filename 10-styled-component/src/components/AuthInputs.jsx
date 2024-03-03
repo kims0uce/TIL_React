f3f7856cfcc522e768003f6814261c50b8e69993
kims0uce <ksy_59@naver.com>
2024-03-03 18:15:49 +0900

    update using styled-component

--- 10-styled-component/src/components/AuthInputs.jsx
+++ 10-styled-component/src/components/AuthInputs.jsx
@@ -1,12 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,36 +46,38 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
-          <label>Email</label>
-          <input
+          <Label $invalid={emailNotValid}>Email</Label>
+          <Input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            $invalid={emailNotValid}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Label $invalid={passwordNotValid}>Password</Label>
+          <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
--- 10-styled-component/src/components/Header.jsx
+++ 10-styled-component/src/components/Header.jsx
@@ -1,18 +1,12 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p
-        style={{
-          textAlign: "center",
-          color: "#a39191",
-          margin: 0,
-        }}
-      >
+      <p className={classes.paragraph}>
         A community of artists and art-lovers.
       </p>
     </header>
