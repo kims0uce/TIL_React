70173e004fa347aeacd74b0dd919807e8a2ed247
kims0uce <ksy_59@naver.com>
2024-03-04 21:45:53 +0900

    adjust styled-component

--- 10-styled-component/src/components/Header.jsx
+++ 10-styled-component/src/components/Header.jsx
@@ -1,14 +1,58 @@
+import { styled } from "styled-components";
+
 import logo from "../assets/logo.png";
-import classes from "./Header.module.css";
+// import classes from "./Header.module.css";
+
+// 같은 파일에 커스텀 컴포넌트를 선언할 수 있다.
+// 가능한 이유는 이 컴포넌트를 오직 이 파일에서만 사용하고,
+// 이 파일과 밀접하게 연관되어 있기 때문이다.
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  & img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    margin-bottom: 4rem;
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: "Pacifico", cursive;
+    margin: 0;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p className={classes.paragraph}>
-        A community of artists and art-lovers.
-      </p>
-    </header>
+      <p>A community of artists and art-lovers.</p>
+    </StyledHeader>
   );
 }
