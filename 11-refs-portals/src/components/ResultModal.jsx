e9375f145f571772dc27d48a2c0f53f116fd5c57
kims0uce <ksy_59@naver.com>
2024-03-13 00:12:02 +0900

    portals 적용

--- 11-refs-portals/src/components/ResultModal.jsx
+++ 11-refs-portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = ({ timeRemain, targetTime, onReset }, ref) => {
   const dialog = useRef();
@@ -16,7 +17,7 @@ const ResultModal = ({ timeRemain, targetTime, onReset }, ref) => {
       },
     };
   });
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
       {userLost ? <h2>You Lost </h2> : <h2>Your Score : {score}</h2>}
 
@@ -30,7 +31,8 @@ const ResultModal = ({ timeRemain, targetTime, onReset }, ref) => {
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 };
 
