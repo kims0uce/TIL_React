9274692deaa2583b41a2d2bab460d581742c40d4
kims0uce <ksy_59@naver.com>
2024-03-24 23:54:00 +0900

    update to use useReducer Hook

--- 12-contextAPI-useReducer/src/store/shopping-cart-context.jsx
+++ 12-contextAPI-useReducer/src/store/shopping-cart-context.jsx
@@ -1,7 +1,108 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
+import { DUMMY_PRODUCTS } from "../dummy-products";
 export const CartContext = createContext({
   items: [],
   addItemToCart: () => {},
   updateItemQuantity: () => {},
 });
+
+// dispatch 함수를 통해 action이 보내진 후
+// React는 Reducer 함수를 호출한다.
+// 이때 state는 리액트의 리듀서가 관리하는 가장 최신 상태의 스냅샷이며,
+// dispatch 를 통해 보내진 action은 Reducer가 받는 두번째 매개변수와 정확히 동일하다.
+function shoppingCartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+  return state;
+}
+
+// 컨텍스트 데이터를 관리하고 노출시키는 함수
+export default function CartContextProvider({ children }) {
+  // dispatch 함수를 통해 action을 보낼 수 있다.
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppingCartReducer,
+    {
+      items: [],
+    }
+  );
+
+  function handleAddItemToCart(id) {
+    // dispatch에 액션을 보낸다.
+    // 보통 속성을 가진 객체를 보내는데, 그 이유는 타입에 따라 다르게 처리하도록 하기 위함이다.
+    shoppingCartDispatch({
+      type: "ADD_ITEM",
+      payload: id,
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: { productId, amount },
+    });
+  }
+
+  // 컨텍스트를 통해 함수도 노출시킬 수 있다.
+  const ctxValue = {
+    items: shoppingCartState.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
