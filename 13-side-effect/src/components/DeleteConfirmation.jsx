6bca5ad71b17b63db27a39818698c098986118a0
kims0uce <ksy_59@naver.com>
2024-04-27 20:18:56 +0900

    add setInterval with useEffect

--- 13-side-effect/src/components/DeleteConfirmation.jsx
+++ 13-side-effect/src/components/DeleteConfirmation.jsx
@@ -1,13 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    // 매 ms마다 실행시키는 함수
+    const interval = setInterval(() => {
+      console.log("INTERVAL");
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   // useEffect를 통해 컴포넌트가 사라질때 setTimeout함수의 실행을 멈춘다.
   useEffect(() => {
     // 3초가 지나면 콜백함수가 실행된다.
     console.log("SET TIMER");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     // useEffect가 리턴하는 함수는
     // effect 함수가 다시 작동하기 바로 전이나, 컴포넌트가 사라지기 바로 전에 실행된다.
@@ -30,6 +45,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
