c89c9b61ca406c6353c7552ca75d54cd93a7d3b3
kims0uce <ksy_59@naver.com>
2024-04-27 15:13:26 +0900

    case do not use useEffect

--- 13-side-effect/src/App.jsx
+++ 13-side-effect/src/App.jsx
@@ -1,15 +1,39 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import { sortPlacesByDistance } from "./loc.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+
+// 아래 두 줄의 코드는 동시에 작동하고,
+// 앱 컴포넌트 함수의 실행이 끝나기를 기다릴 필요가 없다.
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+// 선택한 장소의 상태를 초기화 하는데 사용함.
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
 
 function App() {
   const modal = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  useEffect(() => {
+    // 사용자의 위치를 가져오기 위한 객체
+    navigator.geolocation.getCurrentPosition((position) => {
+      // 사용자의 위치를 확인한 후 브라우저에서 실행시키는 함수
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +52,18 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    // 브라우저의 저장소에 데이터를 저장할 수 있도록 해줌
+    // 로컬스토리지에 저장된 데이터는 새로고침해도 유지된다.
+    // setItem("식별자", 문자열)
+    // 배열은 JSON.stringify로 변환이 필요하다.
+    if (!storedIds.includes(id)) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -35,6 +71,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -57,13 +99,14 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
